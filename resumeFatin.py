87a6cc1a88829fa2510e559bdbf5fe12498e39db
fatinnurasiyah <rhfatin@gmail.com>
2025-10-03 21:06:01 +0800

    Update resumeFatin.py

--- resumeFatin.py
+++ resumeFatin.py
@@ -10,24 +10,24 @@ st.write("Bachelor in Information Technology, University of Malaysia, Kelantan ,
 
 st.header("Work Experience")
 st.write("Internship (Diploma) , Asia Tech Biz Sdn Bhd , 6 months  ")
-st.write("- Assisted in IT-related tasks and gained exposure to real-world business systems.")
+st.write("Assisted in IT-related tasks and gained exposure to real-world business systems.")
 st.write("Crew (F&B) ,  2 years ")
-st.write("- Provided customer service in food & beverage operations.  
-          - Handled orders, ensured customer satisfaction, and maintained service quality.")
+st.write("Provided customer service in food & beverage operations.  
+          Handled orders, ensured customer satisfaction, and maintained service quality.")
           
 st.header("Skills")
-st.write("- Programming Languages:** Java, C++")
-st.write("- Web Development:** HTML, Flutter")
+st.write("Programming Languages:** Java, C++")
+st.write("Web Development:** HTML, Flutter")
 
 st.header("Projects")
-st.write("Security Smart Home Automation (Diploma Project)**  
-          - Controlled home appliances (lights and fans) automatically.  
-          - Integrated alarm system with phone notifications in case of intrusions.  
+st.write("Security Smart Home Automation (Diploma Project) 
+          Controlled home appliances (lights and fans) automatically.  
+          Integrated alarm system with phone notifications in case of intrusions.  
 
-           Automated Vending Machine for Prescription Medication (Degree Group Project)  
-         - Built an IoT-based vending system using ESP32-CAM and QR code.  
-         - Dispensed medicine efficiently to reduce patient waiting times.  
+          Automated Vending Machine for Prescription Medication (Degree Group Project)  
+          Built an IoT-based vending system using ESP32-CAM and QR code.  
+          Dispensed medicine efficiently to reduce patient waiting times.  
 
-           TRIPJR Travel App (Degree Group Assignment)
-         - Developed a Flutter-based travel application.  
-         - Enabled users to search and book nearby hotels or homestays for vacations. """)
+          TRIPJR Travel App (Degree Group Assignment)
+          Developed a Flutter-based travel application.  
+          Enabled users to search and book nearby hotels or homestays for vacations. """)
